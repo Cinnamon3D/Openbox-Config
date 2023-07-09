fe188519a84f158af8aeecd868c12784525c6cb0
Simeon Black <90055606+SimeonBlack@users.noreply.github.com>
2023-07-09 18:10:21 -0500

    Update CAD.sh

--- CAD.sh
+++ CAD.sh
@@ -1,1 +1,6 @@
 sudo apt install -y freecad blender prusa-slicer solvespace
+
+cd
+
+curl -LO "https://github.com/realthunder/FreeCAD/releases/download/Tip/org.freecadweb.FreeCAD.Link.Tip_20230521.flatpak"
+flatpak install org.freecadweb.FreeCAD.Link.Tip_20230521.flatpak
