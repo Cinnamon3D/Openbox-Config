bbaf4ae1289e5f98824f11b47fe8a9ac9d44d668
Simeon Black <90055606+SimeonBlack@users.noreply.github.com>
2023-07-09 16:11:10 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt upgrade
 
-sudo apt install openbox rofi lxappearance polybar alacritty thunar nitrogen nala bluez blueman network-manager i3lock-fancy brightnessctl zip neofetch btop flatpak
+sudo apt install openbox rofi lxappearance polybar alacritty thunar nitrogen nala curl git bluez blueman network-manager i3lock-fancy brightnessctl zip neofetch btop flatpak
 sudo apt install freecad blender prusa-slicer solvespace
 sudo apt install breeze-cursor-theme breeze-gtk-theme gnome-themes-extra 
 
