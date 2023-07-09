df056319a31cbc021b7d5db8fce2438da98bc76a
Simeon Black <90055606+SimeonBlack@users.noreply.github.com>
2023-07-09 16:31:04 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-sudo apt update
-sudo apt upgrade
+sudo apt update -y
+sudo apt upgrade -y
 
-sudo apt install openbox rofi lxappearance polybar alacritty thunar nitrogen nala curl git bluez blueman network-manager i3lock-fancy brightnessctl zip neofetch btop flatpak
-sudo apt install freecad blender prusa-slicer solvespace
-sudo apt install breeze-cursor-theme breeze-gtk-theme gnome-themes-extra 
+sudo apt install -y openbox rofi lxappearance polybar alacritty thunar nitrogen nala curl git bluez blueman network-manager i3lock-fancy brightnessctl zip neofetch btop flatpak
+sudo apt install -y freecad blender prusa-slicer solvespace
+sudo apt install -y breeze-cursor-theme breeze-gtk-theme gnome-themes-extra 
 
 mkdir ~/.config/polybar
 mv config.ini ~/.config/polybar
@@ -27,5 +27,4 @@ sudo apt install ./google-chrome-stable_current_amd64.deb
 curl -LO "https://github.com/realthunder/FreeCAD/releases/download/Tip/org.freecadweb.FreeCAD.Link.Tip_20230521.flatpak"
 flatpak install org.freecadweb.FreeCAD.Link.Tip_20230521.flatpak
 
-rofi-theme-selector
-
+echo "run rofi-theme-selector, obconf, lxappearance, and nitrogen"
