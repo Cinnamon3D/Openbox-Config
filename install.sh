6911859615bd00a9bbe2e73d6b94207dcac84f39
Simeon Black <90055606+SimeonBlack@users.noreply.github.com>
2023-07-09 17:56:07 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,22 +7,20 @@ sudo apt install -y breeze-cursor-theme breeze-gtk-theme gnome-themes-extra
 
 sudo apt remove -y xterm vim*
 
-INSTALL_DIR="$HOME/openbox-configs"
-
 mkdir "$HOME/.config/polybar"
-mv "$INSTALL_DIR/config.ini" "$HOME/.config/polybar"
+mv "config.ini" "$HOME/.config/polybar"
 
 mkdir -p "$HOME/.config/openbox"
-mv "$INSTALL_DIR/autostart" "$HOME/.config/openbox"
-mv "$INSTALL_DIR/autostart" "$HOME/.config/openbox"
+mv "autostart" "$HOME/.config/openbox"
+mv "rx.xml" "$HOME/.config/openbox"
 
 mkdir -p "$HOME/.config/alacritty"
-mv "$INSTALL_DIR/alacritty.yml" "$HOME/.config/alacritty"
+mv "alacritty.yml" "$HOME/.config/alacritty"
 
 mkdir -p "$HOME/.config/neofetch"
-mv "$INSTALL_DIR/config.conf" "$HOME/.config/neofetch"
+mv "config.conf" "$HOME/.config/neofetch"
 
-sudo mv "$INSTALL_DIR/sources.list" "/etc/apt"
+sudo mv "sources.list" "/etc/apt"
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
@@ -33,5 +31,7 @@ sudo apt install -y ./google-chrome-stable
 
 curl -LO "https://github.com/realthunder/FreeCAD/releases/download/Tip/org.freecadweb.FreeCAD.Link.Tip_20230521.flatpak"
 flatpak install org.freecadweb.FreeCAD.Link.Tip_20230521.flatpak
+
+sudo apt autoremove -y
 
 echo "run rofi-theme-selector, obconf, lxappearance, and nitrogen"
