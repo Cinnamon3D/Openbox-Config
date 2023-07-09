42b0ed47d5a2cd14d275f740a886586acb0c66ed
Simeon Black <90055606+SimeonBlack@users.noreply.github.com>
2023-07-09 15:44:27 -0500

    Update and rename .config to install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt upgrade
+
+sudo apt install openbox rofi lxappearance polybar alacritty thunar nitrogen nala bluez blueman network-manager i3lock-fancy brightnessctl zip neofetch btop flatpak
+sudo apt install freecad blender prusa-slicer solvespace
+sudo apt install breeze-cursor-theme breeze-gtk-theme gnome-themes-extra 
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
